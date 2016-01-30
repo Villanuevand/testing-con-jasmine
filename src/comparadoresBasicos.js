5e2870c27bd7b2806c674145f27a5a3ffce2012b
Villanuevand <andresvillanuevab@gmail.com>
2016-01-30 15:53:43 -0300

    Objeto a utilizar

---
+++ src/comparadoresBasicos.js
@@ -0,0 +1,8 @@
+var Comparadores = {
+	jasmine : 'Jasmine Cookbook',
+	telefono : '001-002-03-04',
+	unNumero : 16,
+	lorem : 'Lorem ipsum dolor',
+	unObjeto : {a : 50, b: 60},
+	unArray : [2,4,8]
+};
