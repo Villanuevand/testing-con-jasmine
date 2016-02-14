532010132247a0d28441db60e9856bf8421e7738
Villanuevand <andresvillanuevab@gmail.com>
2016-02-14 20:38:37 -0300

    Correcion en prueba

--- spec/comparadoresBasicos_spec.js
+++ spec/comparadoresBasicos_spec.js
@@ -10,20 +10,14 @@ describe('Jasmine Matchers (Comparadores) Básicos', function() {
 	});
 
 	describe('Comparador "toEqual()"', function() {
-		it('Si los números son iguales', function() {
+		it('"toBe deberia ser aplicado para comparar variables literales y objetos sean de igual valor', function() {
 			expect(Comparadores.unNumero).toEqual(16);
-		});
-		it('Si las cadenas string son iguales', function() {
 			var strVar = 'Jasmine Cookbook';
 			expect(Comparadores.jasmine).toEqual('Jasmine Cookbook');
-		});
-		it('Si los objetos son iguales', function() {
 			var objetoTest = {a: 50, b: 60};
 			expect(Comparadores.unObjeto).toEqual(objetoTest);
 			expect(Comparadores.unObjeto.a).toEqual(objetoTest.a);
 			expect(Comparadores.unObjeto.a).not.toEqual(objetoTest.b);
-		});
-		it('Si los arreglos son iguales', function() {
 			var arregloTest = [2,4,8];
 			expect(Comparadores.unArray).toEqual(arregloTest);
 			expect(Comparadores.unArray).not.toEqual([8, 9, 10]);
@@ -31,63 +25,39 @@ describe('Jasmine Matchers (Comparadores) Básicos', function() {
 	});
 
 	describe('Comparador "toBe()"', function() {
-		var objetoTest = {a: 50, b: 60};
-		var arregloTest = [2,4,8];
-		it('Si el objeto es él mismo', function() {
+		it('"toBe deberia ser aplicado para comparar variables literales y objetos sean el mismo objeto.', function() {
+			var objetoTest = {a: 50, b: 60};
+			var arregloTest = [2,4,8];
 			expect(Comparadores.unObjeto).toBe(Comparadores.unObjeto);
-		});
-		it('Si el objeto no es él mismo', function() {
 			expect(Comparadores.unObjeto).not.toBe(objetoTest);
-		});
-		it('Deberian ser iguales', function() {
 			expect(Comparadores.unObjeto).toEqual(objetoTest);
-		});
-		it('Deberian se igual en valor', function() {
 			expect(Comparadores.jasmine).toBe("Jasmine Cookbook");
-		});
-		it('Son iguales en valor', function() {
 			expect(Comparadores.unArray).toEqual([2, 4, 8]);
-		});
-		it('No  deberian ser iguales en tipo', function() {
 			expect(Comparadores.unArray).not.toBe([8, 9, 10]);
 		});
 				
 	});
 	describe('Comparador "toBeDefined"()', function() {
-		var fakeVar;
-		it('Si "Jasmine Cookbook" está definido', function() {
+		it('"toBeDefined deberia ser aplicado para comparar contra "defined"', function() {
+			var fakeVar;
 			expect('Jasmine Cookbook').toBeDefined();
-		});
-		it('Si el objeto "Comparadores" esá definido', function() {
 			expect(Comparadores).toBeDefined();
-		});
-		it('Si Comparadores.unNumero está definido', function() {
 			expect(Comparadores.unNumero).toBeDefined();
-		});
-		it('Si Comparadores.unFuncion NO está definido (undefined)', function() {
 			expect(Comparadores.unaFuncion).not.toBeDefined();
-		});
-		it('Si la variable "fakeVariable no está definida"', function() {
 			expect(fakeVar).not.toBeDefined();
 		});
 	});
 	describe('Comparador "toBeUndefined()"', function() {
-		var fakeVar;
-		it('Si el objeto "Comparadores" no es indefinido', function() {
+		it('"toBeUndefined" deberia ser aplicado para compara contrar conta "undefined"', function() {
+			var fakeVar;
 			expect(Comparadores).not.toBeUndefined();
-		});
-		it('Si Comparadores.unNumero no es indefinido', function() {
 			expect(Comparadores.unNumero).not.toBeUndefined();
-		});
-		it('Si Comparadores.unFuncion es indefinido', function() {
 			expect(Comparadores.unaFuncion).toBeUndefined();
-		});
-		it('Si la variable "fakeVariable es indefinida"', function() {
 			expect(fakeVar).toBeUndefined();
 		});
 	});
 	describe('Comparador "toBeNull()', function() {
-		it('Deberia ser aplicado para comprar contra "null"', function() {
+		it('Deberia ser aplicado para comparar contra "null"', function() {
 			var valueUndefined;
 			expect(null).toBeNull();
 			expect(Comparadores.nulleable).toBeNull();
