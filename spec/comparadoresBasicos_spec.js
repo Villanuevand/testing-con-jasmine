5069b7b87348f40557f837916cd006f470f6f1c2
Andrés Villanueva <andresvillanuevab@gmail.com>
2016-02-15 12:01:56 -0300

    Comparador toBeCloseTo

--- spec/comparadoresBasicos_spec.js
+++ spec/comparadoresBasicos_spec.js
@@ -114,8 +114,16 @@ describe('Jasmine Matchers (Comparadores) Básicos', function() {
 		});
 	});
 	describe('Comparador "toBeCloseTo()"', function() {
-		it('"toBe" deberia ser aplicado para comparar', function() {
-			
+		it('"toBeCloseTo" deberia ser aplicado para comparaciones matemáticas precisas.', function() {
+			var pi = Math.PI, e = 2.78;
+			expect(pi).not.toBeCloseTo(e);
+			expect(pi).toBeCloseTo(e,0);
+			expect(4.334).toBeCloseTo(4.334);
+			expect(4.334).toBeCloseTo(4.3345,1);
+			expect(4.334).toBeCloseTo(4.3345,2);
+			expect(4.334).not.toBeCloseTo(4.3,2);
+			expect(4.223).not.toBeCloseTo(4.22,3);
+			expect(4.223).not.toBeCloseTo(4.22,4);
 		});
 	});
 
